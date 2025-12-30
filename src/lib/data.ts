872f6d845b73b34322d62d97805fcd5b63614331
Von Garcia <vongarcia97@gmail.com>
2025-12-30 07:40:59 -0500

    minor updates to about section

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -54,7 +54,7 @@ const Experience = [
 const Interests = [
   {
     id: 1,
-    title: "Weekend Chef",
+    title: "Weekend Chef 🍳",
     subtitle: "Trying out my take on different cuisines on the weekends",
     description:
       "Whatever I see that makes my mouth water on my Instagram Discover Page or TikTok algo, I try to make it. To give an idea of how daring I am - I've made bao buns from scratch and prepare my own sashimi at home.",
@@ -63,8 +63,8 @@ const Interests = [
   },
   {
     id: 2,
-    title: "Formula One - McLaren Racing",
-    subtitle: "What can I say? Netflix'sDrive To Survive is just THAT good....",
+    title: "Formula One - McLaren Racing 🏎️",
+    subtitle: "COVID-19 + Netflix's Drive To Survive.... It was inevitable....",
     description:
       "I've always admired the engineering that goes into making a Formula One car. I chose to support McLaren because that team has pedigree and I want to see them back on top! I'm excited to see Lando Norris and McLaren challenge Red Bull for the Drivers' and Constructors' Championships.",
     date: "Since 2020",
@@ -72,7 +72,7 @@ const Interests = [
   },
   {
     id: 3,
-    title: "Liverpool FC",
+    title: "Liverpool FC ⚽",
     subtitle: "You'll Never Walk Alone!",
     description:
       "I started following Liverpool FC because of their history and the fact that they were the underdogs. I've been a fan since 2018, when we lost to Real Madrid on the Champion's League Final. I've been through the highs and lows of the team. I hated seeing Jurgen Klopp leave, but I'm excited to see what the future holds for the team!",
@@ -81,11 +81,11 @@ const Interests = [
   },
   {
     id: 4,
-    title: "Traveling",
+    title: "Traveling 🌍 🛫",
     subtitle:
-      "Working in the travel industry doesn't help with my travel bug....",
+      "I had no choice, but working in the leisure travel definitely doesn't help with my travel bug....",
     description:
-      "I was born and raised in the Philippines, and I have always been fascinated by different cultures ever since I migrated to the United States and moved to NYC. I am also a self-proclaimed 'points hack' specialist, my latest steal was two Premium Comfort seats to Rome for around60k points! I'm blessed to have visited beautiful places such as Paris, Nice, Bordeaux, Barcelona, Priorat, Rome, Milan, etc. - but Gozo, Malta is what I consider to be paradise.",
+      "I grew up in the Philippines, moved to the US during my teen years and now live in NYC🗽. I am a self-proclaimed 'AMEX points redemption specialist' since I've been redeeming points for travel for the last five years. My latest steal were 2 Air France Premium Comfort seats to Rome for ~60k points! I'm blessed to have visited beautiful cities like Paris, Nice, Bordeaux, Barcelona, Priorat, Rome, Milan, etc. - but the tiny island of Gozo in Malta is what I consider to be paradise! 🌴🌤️",
     date: "Always been and always will be a nomad!",
     icon: React.createElement(FaPlane),
   },
